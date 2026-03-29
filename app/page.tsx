2866fe600c3c5380266f63815641c3b827035cba
Your Name <you@example.com>
2026-03-29 14:38:15 +0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <div>
         <h1>name is murshide</h1>
+        <Button>Text schcni </Button>
       </div>
     </>
   );
