b5cb1e5df7496ac2cdbda2fd43a0609cd675bef2
Your Name <you@example.com>
2026-03-29 18:26:38 +0500

    fix schema issues

--- app/(public)/blog/[slug]/page.tsx
+++ app/(public)/blog/[slug]/page.tsx
@@ -3,21 +3,25 @@ import { getPostBySlug } from "@/actions/post.actions";
 export default async function BlogDetail({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }) {
-  const post = await getPostBySlug(params.slug);
+  const post = await getPostBySlug((await params).slug);
 
   if (!post) return <div>Post not found</div>;
 
   return (
     <div className="container py-10">
-      <h1 className="text-3xl font-bold">{post.title}</h1>
+      <h1 className="text-3xl font-bold">{post.data?.title}</h1>
 
-      {post.coverImage && (
-        <img src={post.coverImage} alt={post.title} className="my-6" />
+      {post.data?.coverImage && (
+        <img
+          src={post.data.coverImage}
+          alt={post.data.title}
+          className="my-6"
+        />
       )}
 
-      <p>{post.content}</p>
+      <p>{post.data?.content}</p>
     </div>
   );
 }
